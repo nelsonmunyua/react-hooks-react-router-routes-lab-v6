dc85eecb4550969d5797b437896fb9eacf75dc33
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-10-23 04:50:29 +0300

    lab complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
-import "./index.css";
-
-const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render();
